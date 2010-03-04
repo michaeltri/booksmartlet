e5619c8b92805ea55ba1f1029d219e37795980a1
Michael Tritschler <michael.tri7@gmail.com>
2010-03-04 22:45:28 +0100

    fixes

--- booksmartlet.js
+++ booksmartlet.js
@@ -1,19 +1,31 @@
 javascript:
-var bsl_c = window.prompt('Booksmartlet:');
-var bsl_u = 'http://github.com/michaeltri/booksmartlet/raw/master/';
+var gBooksmartletParam1 = '';
+var gBooksmartletParam2 = '';
 
-if (bsl_c)
+Booksmartlet();
+function Booksmartlet()
 {
-    q = document.getElementById('booksmartlet');
-    if (q) 
+    var url = 'http://github.com/michaeltri/booksmartlet/raw/master/';
+    var input = window.prompt('Booksmartlet:');
+    
+    if (input)
     {
-        q.parentNode.removeChild(q);
-    }
+        var params = input.split(" ");
+        cmd = params[0];
+        gBooksmartletParam1 = params[1];
+        gBooksmartletParam2 = params[2];
         
-    sc = document.createElement('script');
-    sc.src = bsl_u + bsl_c + '.js';
-    sc.id = 'booksmartlet';
-    sc.type = 'text/javascript';
-    heads = document.getElementsByTagName('head');
-    void(heads[0].appendChild(sc));
+        q = document.getElementById('booksmartlet');
+        if (q) 
+        {
+            q.parentNode.removeChild(q);
+        }
+        
+        sc = document.createElement('script');
+        sc.src = url + cmd + '.js';
+        sc.id = 'booksmartlet';
+        sc.type = 'text/javascript';
+        heads = document.getElementsByTagName('head');
+        void(heads[0].appendChild(sc));
+    }
 }
