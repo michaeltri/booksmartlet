9a2821fbdf4f24733d52423ffdf98dcb8bd4a41f
Michael Tritschler <michael.tri7@gmail.com>
2010-03-18 22:12:34 +0100

    j.mp fixes

--- jmp.js
+++ jmp.js
@@ -3,9 +3,7 @@ function execBooksmartlet(param1, param2)
     setTimeout(
         function()
         {
-            var d=document,w=window,enc=encodeURIComponent,e=w.getSelection,k=d.getSelection,x=d.selection,
-                s=(e?e():(k)?k():(x?x.createRange().text:0)),s2=((s.toString()=='')?s:('%22'+enc(s)+'%22')),
-                f='http://j.mp/',l=d.location,p='?v=3&u='+enc(l.href)%20+'&s='+enc(d.title)+'%20'+s2,u=f+p;
+            var d=document,w=window,enc=encodeURIComponent,e=w.getSelection,k=d.getSelection,x=d.selection,s=(e?e():(k)?k():(x?x.createRange().text:0)),s2=((s.toString()=='')?s:('%22'+enc(s)+'%22')),f='http://j.mp/',l=d.location,p='?v=3&u='+enc(l.href)%20+'&s='+enc(d.title)+'%20'+s2,u=f+p;
             try
             {
                 if(!/^(.*\.)?tumblrzzz[^.]*$/.test(l.host))throw(0);
