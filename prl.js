28000cbb97a2c536b21d2a1948622e358da0fa46
Michael Tritschler <michael.tri7@gmail.com>
2010-08-01 17:25:13 +0200

    Fixes

--- prl.js
+++ prl.js
@@ -3,6 +3,5 @@ function execBooksmartlet()
     q=location.href;
     p=document.title;
     t=open('http://pinboard.in/add?later=yes&noui=yes&jump=close&url='+encodeURIComponent(q)+'&title='+encodeURIComponent(p),'Pinboard','toolbar=no,width=100,height=100'));
-    t.blur();
 }
 execBooksmartlet();
