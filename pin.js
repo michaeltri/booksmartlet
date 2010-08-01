a9c78dcc2c6d3083684f4ecd7c9dec037f680f77
Michael Tritschler <michael.tri7@gmail.com>
2010-08-01 16:07:59 +0200

    update

--- pin.js
+++ pin.js
@@ -1,10 +1,1 @@
-function execBooksmartlet(param1, param2)
-{
-    setTimeout(
-        function()
-        {
-            location.href='http://pinboard.in/u:michaeltri';
-        },
-        0)
-}
-execBooksmartlet(gBooksmartletParam1, gBooksmartletParam2);
+http://pinboard.in/u:michaeltri
--- txt.js
+++ txt.js
@@ -1,10 +1,5 @@
-function execBooksmartlet(param1, param2)
+function execBooksmartlet()
 {
-    setTimeout(
-        function()
-        {
-            location.href='http://www.aaronsw.com/2002/html2text/?url='+document.location.href;
-        },
-        0)
+    location.href='http://www.aaronsw.com/2002/html2text/?url='+document.location.href;
 }
-execBooksmartlet(gBooksmartletParam1, gBooksmartletParam2);
+execBooksmartlet();
